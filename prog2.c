13e087df179c0b22315a18ddc6e7d57c6c8a4266
ChxirMac <zyryanov2005@gmail.com>
2025-04-18 14:55:25 +0700

    corrected

--- prog2.c
+++ prog2.c
@@ -5,9 +5,13 @@
 #include <unistd.h>
 #include <string.h>
 #include <dirent.h>
-//qwe
-
+#include <libgen.h>
 #define SIZE 4096
+
+enum ERRORS {
+    SUCCESS = 0,
+    USAGE_ERROR = 1
+};
 
 void myMkdir(char *path, mode_t mode) {
     if (mkdir(path, mode) == -1) {
@@ -42,11 +46,9 @@ int myRmdir(char *path) {
             if (!strcmp(files->d_name, ".") || !strcmp(files->d_name, "..")) {
                 continue;
             }
-
             char *fullPath;
             len = pathLen + strlen(files->d_name) + 2;
             fullPath = malloc(len);
-
             if (fullPath) {
                 struct stat st;
                 snprintf(fullPath, len, "%s/%s", path, files->d_name);
@@ -152,7 +154,7 @@ void myLsl(char *path) {
         printf( (sb.st_mode & S_IXOTH) ? "x" : "-");
         printf("\n\n");
 
-        printf("Number of hard links: %ld\n", sb.st_nlink);
+        printf("Number of hard links: %d\n", sb.st_nlink);
     }
 }
 
@@ -164,77 +166,78 @@ void myChmod(char *path, mode_t mode) {
 
 int main(int argc, char **argv) {
     if (argc < 2) {
-        return -1;
+        printf("First create hard link with one of the commands: mkdir, ls, rmdir, touch, cat, rm, lnSym, readlink, catLinkContent, rmSymlink, ln, rmHardLink, lsl, chmod.\nUsage: ./comand <path>");
+        return USAGE_ERRORll
     }
-    char *action = argv[0];
+    char *action = basename(argv[0]);
     char *path = argv[1];
 
-    if (strcmp(action, "./mkdir") == 0) {
+    if (strcmp(action, "mkdir") == 0) {
         if (argc != 3) {
             printf("mkdir <path> <mode>\n");
-            return -1;
+            return USAGE_ERROR;
         }
         mode_t mode = strtol(argv[2], NULL, 8);
         myMkdir(path, mode);
     }
-    else if (strcmp(action, "./ls") == 0) {
+    else if (strcmp(action, "ls") == 0) {
         myLs(path);
-    } 
-    else if (strcmp(action, "./rmdir") == 0) {
+    }
+    else if (strcmp(action, "rmdir") == 0) {
         myRmdir(path);
     }
-    else if (strcmp(action, "./touch") == 0) {
+    else if (strcmp(action, "touch") == 0) {
         myTouch(path);
     }
-    else if (strcmp(action, "./cat") == 0) {
+    else if (strcmp(action, "cat") == 0) {
         myCat(path);
-    } 
-    else if (strcmp(action, "./rm") == 0) {
+    }
+    else if (strcmp(action, "rm") == 0) {
         myRm(path);
-    } 
-    else if (strcmp(action, "./lnSym") == 0) {
+    }
+    else if (strcmp(action, "lnSym") == 0) {
         if (argc != 3) {
             printf("ln <target> <link>\n");
-            return -1;
+            return USAGE_ERROR;
         }
         char *destPath = argv[1];
         char *linkPath = argv[2];
         mySymLn(destPath, linkPath);
-    } 
-    else if (strcmp(action, "./readlink") == 0) {
+    }
+    else if (strcmp(action, "readlink") == 0) {
         myReadlink(path);
     }
-    else if (strcmp(action, "./catLinkContent") == 0) {
+    else if (strcmp(action, "catLinkContent") == 0) {
         catLinkContent(path);
     }
-    else if (strcmp(action, "./rmSymLink") == 0) {
+    else if (strcmp(action, "rmSymLink") == 0) {
         myRm(path);
     }
-    else if (strcmp(action, "./ln") == 0) {
-        if (argc < 4) {
-            printf("ln <path> <mode>\n");
-            return 1;
+    else if (strcmp(action, "ln") == 0) {
+        if (argc != 3) {
+            printf("ln <target> <link>\n");
+            return USAGE_ERROR;
         }
         char *destPath = argv[1];
         char *linkPath = argv[2];
         myLn(destPath, linkPath);
     }
-    else if (strcmp(action, "./rmHardLink") == 0) {
+    else if (strcmp(action, "rmHardLink") == 0) {
         myRm(path);
-    } 
-    else if (strcmp(action, "./lsl") == 0) {
+    }
+    else if (strcmp(action, "lsl") == 0) {
         myLsl(path);
     }
-    else if (strcmp(action, "./cmod") == 0) {
-        if (argc != 2) {
-            printf("cmod <target> <link>\n");
-            return 1;
+    else if (strcmp(action, "chmod") == 0) {
+        if (argc != 3) {
+            printf("chmod <path> <mode>\n");
+            return USAGE_ERROR;
         }
         mode_t mode = strtol(argv[2], NULL, 8);
         myChmod(path, mode);
-    } 
-    else {
-        printf("error");
     }
-    return 0;
+    else {
+        printf("unknown comand");
+    }
+    return SUCCESS;
 }
