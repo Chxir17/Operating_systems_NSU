b7fa6b5effa995f0c59207b6976e7f6d04a9e1e6
Chxir17 <145639047+Chxir17@users.noreply.github.com>
2025-05-14 22:04:18 +0700

    Update prog2.c

--- lab3/prog2.c
+++ lab3/prog2.c
@@ -6,7 +6,10 @@
 #include <string.h>
 #include <dirent.h>
 #include <libgen.h>
+#include <errno.h>
+
 #define SIZE 4096
+
 
 enum ERRORS {
     SUCCESS = 0,
@@ -15,166 +18,167 @@ enum ERRORS {
 
 void myMkdir(char *path, mode_t mode) {
     if (mkdir(path, mode) == -1) {
-        printf("Error! Can't create directory.");
+        perror("mkdir error");
     }
 }
 
 void myLs(char *path) {
-    DIR *dir;
+    DIR *dir = opendir(path);
+    if (!dir) {
+        perror("opendir error");
+        return;
+    }
     struct dirent *files;
-    dir = opendir(path);
-    if (dir) {
-        while ((files = readdir(dir)) != NULL) {
-            printf("%s\n", files->d_name);
-        }
-        closedir(dir);
+    while ((files = readdir(dir)) != NULL) {
+        printf("%s\n", files->d_name);
     }
-    else {
-        printf("Error! Directory does not exist.");
-    }
+    closedir(dir);
 }
 
 int myRmdir(char *path) {
     DIR *dir = opendir(path);
-    int success = -1;
-    if (dir) {
-        struct dirent *files;
-        long len, pathLen = strlen(path);
-        success = 0;
-        while ((files = readdir(dir)) && !success) {
-            int success2 = -1;
-            if (!strcmp(files->d_name, ".") || !strcmp(files->d_name, "..")) {
-                continue;
-            }
-            char *fullPath;
-            len = pathLen + strlen(files->d_name) + 2;
-            fullPath = malloc(len);
-            if (fullPath) {
-                struct stat st;
-                snprintf(fullPath, len, "%s/%s", path, files->d_name);
-                if (!stat(fullPath, &st)) {
-                    if (S_ISDIR(st.st_mode)){
-                        success2 = myRmdir(fullPath);
-                    }
-                    else{
-                        success2 = unlink(fullPath);
-                    }
-                }
-                free(fullPath);
-            }
-            success = success2;
-        }
-        closedir(dir);
-    }
-    else {
-        printf("Error! Directory does not exist.");
+    if (!dir) {
+        perror("opendir error");
+        return -1;
     }
 
-    if (!success){
-        success = rmdir(path);
+    struct dirent *files;
+    int success = 0;
+    while ((files = readdir(dir))) {
+        if (!strcmp(files->d_name, ".") || !strcmp(files->d_name, "..")) continue;
+
+        char fullPath[SIZE];
+        snprintf(fullPath, sizeof(fullPath), "%s/%s", path, files->d_name);
+
+        struct stat st;
+        if (stat(fullPath, &st) == -1) {
+            perror("stat error");
+            success = -1;
+            continue;
+        }
+
+        if (S_ISDIR(st.st_mode)) {
+            if (myRmdir(fullPath) == -1) success = -1;
+        } else {
+            if (unlink(fullPath) == -1) {
+                perror("unlink error");
+                success = -1;
+            }
+        }
+    }
+    closedir(dir);
+    if (success == 0 && rmdir(path) == -1) {
+        perror("rmdir error");
+        return -1;
     }
     return success;
 }
 
 void myTouch(char *path) {
     FILE *file = fopen(path, "w");
+    if (!file) {
+        perror("fopen error");
+        return;
+    }
     fclose(file);
 }
 
 void myCat(char *path) {
     FILE *file = fopen(path, "r");
-    int c;
-    if (file) {
-        while ((c = fgetc(file)) != EOF) {
-            putchar(c);
-        }
+    if (!file) {
+        perror("fopen error");
+        return;
     }
-    else {
-        printf("Error! File doesn't exist.");
+
+    int c;
+    while ((c = fgetc(file)) != EOF) {
+        putchar(c);
     }
     fclose(file);
 }
 
 void myRm(char *path) {
     if (remove(path) == -1) {
-        printf("Error! Can't delete this file.");
+        perror("remove error");
     }
 }
 
 void mySymLn(char *destPath, char *linkPath) {
-    symlink(destPath, linkPath);
+    if (symlink(destPath, linkPath) == -1) {
+        perror("symlink error");
+    }
 }
 
 void myReadlink(char *path) {
     char buf[SIZE];
-    long len = readlink(path, buf, sizeof(buf) - 1);
-    if (len != -1) {
-        buf[len] = '\0';
-        printf("%s\n", buf);
+    ssize_t len = readlink(path, buf, sizeof(buf) - 1);
+    if (len == -1) {
+        perror("readlink error");
+        return;
     }
-    else {
-        printf("Error! Symbolic link does not exist.");
-    }
+    buf[len] = '\0';
+    printf("%s\n", buf);
 }
 
 void catLinkContent(char *path) {
     char buf[SIZE];
-    long len = readlink(path, buf, sizeof(buf) - 1);
-    if (len != -1) {
-        buf[len] = '\0';
-        myCat(buf);
+    ssize_t len = readlink(path, buf, sizeof(buf) - 1);
+    if (len == -1) {
+        perror("readlink error");
+        return;
     }
-    else {
-        printf("Error! Symbolic link does not exist.");
-    }
+    buf[len] = '\0';
+    myCat(buf);
 }
 
 void myLn(char *destPath, char *linkPath) {
-    link(destPath, linkPath);
+    if (link(destPath, linkPath) == -1) {
+        perror("link error");
+    }
 }
-
 
 void myLsl(char *path) {
     struct stat sb;
     if (stat(path, &sb) == -1) {
-        printf("Error! Entity does not exist.");
+        perror("stat error");
+        return;
     }
-    else {
-        printf("Permissions: %o\n", sb.st_mode & 0777);
-        printf("File Permissions: \t");
-        printf( (S_ISDIR(sb.st_mode)) ? "d" : "-");
-        printf( (sb.st_mode & S_IRUSR) ? "r" : "-");
-        printf( (sb.st_mode & S_IWUSR) ? "w" : "-");
-        printf( (sb.st_mode & S_IXUSR) ? "x" : "-");
-        printf( (sb.st_mode & S_IRGRP) ? "r" : "-");
-        printf( (sb.st_mode & S_IWGRP) ? "w" : "-");
-        printf( (sb.st_mode & S_IXGRP) ? "x" : "-");
-        printf( (sb.st_mode & S_IROTH) ? "r" : "-");
-        printf( (sb.st_mode & S_IWOTH) ? "w" : "-");
-        printf( (sb.st_mode & S_IXOTH) ? "x" : "-");
-        printf("\n\n");
 
-        printf("Number of hard links: %d\n", sb.st_nlink);
-    }
+    printf("Permissions: %o\n", sb.st_mode & 0777);
+    printf("File Permissions: \t");
+    printf((S_ISDIR(sb.st_mode)) ? "d" : "-");
+    printf((sb.st_mode & S_IRUSR) ? "r" : "-");
+    printf((sb.st_mode & S_IWUSR) ? "w" : "-");
+    printf((sb.st_mode & S_IXUSR) ? "x" : "-");
+    printf((sb.st_mode & S_IRGRP) ? "r" : "-");
+    printf((sb.st_mode & S_IWGRP) ? "w" : "-");
+    printf((sb.st_mode & S_IXGRP) ? "x" : "-");
+    printf((sb.st_mode & S_IROTH) ? "r" : "-");
+    printf((sb.st_mode & S_IWOTH) ? "w" : "-");
+    printf((sb.st_mode & S_IXOTH) ? "x" : "-");
+    printf("\n\n");
+
+    printf("Number of hard links: %ld\n", (long)sb.st_nlink);
 }
 
 void myChmod(char *path, mode_t mode) {
     if (chmod(path, mode) == -1) {
-        printf("Error! Can't change permissions on file.");
+        perror("chmod error");
     }
 }
 
+
 int main(int argc, char **argv) {
     if (argc < 2) {
-        printf("First create hard link with one of the commands: mkdir, ls, rmdir, touch, cat, rm, lnSym, readlink, catLinkContent, rmSymlink, ln, rmHardLink, lsl, chmod.\nUsage: ./comand <path>");
-        return USAGE_ERROR
+        fprintf(stderr, "Usage: ./command <path> [...]\n Commands: mkdir, ls, rmdir, touch, cat, rm, lnSym, readlink,\n catLinkContent, rmSymLink, ln, rmHardLink, lsl, chmod\n");
+        return USAGE_ERROR;
     }
     char *action = basename(argv[0]);
     char *path = argv[1];
 
     if (strcmp(action, "mkdir") == 0) {
         if (argc != 3) {
-            printf("mkdir <path> <mode>\n");
+            fprintf(stderr, "Usage: mkdir <path> <mode>\n");
             return USAGE_ERROR;
         }
         mode_t mode = strtol(argv[2], NULL, 8);
@@ -197,7 +201,7 @@ int main(int argc, char **argv) {
     }
     else if (strcmp(action, "lnSym") == 0) {
         if (argc != 3) {
-            printf("ln <target> <link>\n");
+            fprintf(stderr, "Usage: lnSym <target> <link>\n");
             return USAGE_ERROR;
         }
         char *destPath = argv[1];
@@ -215,7 +219,7 @@ int main(int argc, char **argv) {
     }
     else if (strcmp(action, "ln") == 0) {
         if (argc != 3) {
-            printf("ln <target> <link>\n");
+            fprintf(stderr, "Usage: ln <target> <link>\n");
             return USAGE_ERROR;
         }
         char *destPath = argv[1];
@@ -230,14 +234,15 @@ int main(int argc, char **argv) {
     }
     else if (strcmp(action, "chmod") == 0) {
         if (argc != 3) {
-            printf("chmod <path> <mode>\n");
+            fprintf(stderr, "Usage: chmod <path> <mode>\n");
             return USAGE_ERROR;
         }
         mode_t mode = strtol(argv[2], NULL, 8);
         myChmod(path, mode);
     }
     else {
-        printf("unknown comand");
+        fprintf(stderr, "Unknown command: %s\n", action);
+        return USAGE_ERROR;
     }
     return SUCCESS;
 }
