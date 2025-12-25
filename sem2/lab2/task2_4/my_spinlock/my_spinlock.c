c2a58f39482f7474ec541eb1a4eb4021f0caa147
ChxirMac <zyryanov2005@gmail.com>
2025-12-25 17:10:10 +0700

    fix

--- sem2/lab2/task2_4/my_spinlock/my_spinlock.c
+++ sem2/lab2/task2_4/my_spinlock/my_spinlock.c
@@ -19,7 +19,7 @@ void my_spinlock_init(my_spinlock_t *lock) {
 void my_spinlock_lock(my_spinlock_t *lock) {
     while (1) {
         int expected = 1;
-        if (atomic_compare_exchange(lock, &expected, 0)) {
+        if (atomic_compare_exchange_weak(lock, &expected, 0)) {
             break;
         }
     }
@@ -27,5 +27,5 @@ void my_spinlock_lock(my_spinlock_t *lock) {
 
 void my_spinlock_unlock(my_spinlock_t *lock) {
     const int expected = 0;
-    atomic_compare_exchange(lock, &expected, 1);
+    atomic_compare_exchange_weak(lock, &expected, 1);
 }
